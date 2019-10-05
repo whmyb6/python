a4ad211960a1309bc58079ebc7daee8d7174cf36
whmyb6 <40862811+whmyb6@users.noreply.github.com>
2019-10-05 10:26:21 +0800

    '更新'
    Signed-off-by: whmyb6 <40862811+whmyb6@users.noreply.github.com>

--- movefile.py
+++ movefile.py
@@ -2,9 +2,18 @@
 # -*- coding: UTF-8 -*-
 
 import os
-for root, dirs, files in os.walk(r'J:\OK-2017\OK\JP', topdown=True):
-    #print(format('root = %s')%root)
+import shutil
+
+
+print os.getcwd() #获取当前工作目录路径
+path = r'J:\OK-2017\OK\JP\大橋未久 @@@@@@@@'
+
+path= unicode(path, "utf8")  # 处理中文目录名称 ====
+os.chdir(path)
+print os.getcwd().decode(encoding='gbk')
+for root, dirs, files in os.walk('.', topdown=True):
+    #print(format('root = %s')%root).decode(encoding='gbk')
     for name in files:
-        print(format('filename = %s') % os.path.join(root, name))
-    for name in dirs:
-        print(format('dir = %s') % os.path.join(root, name)).decode(encoding='gbk')
+        print(format('filename = %s') % os.path.join(root, name)).decode(encoding='gbk')
+    #for name in dirs:
+    #    print(format('dir = %s') % os.path.join(root, name)).decode(encoding='gbk')
