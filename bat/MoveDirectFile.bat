6bdc049818b5451a4783f104b3ecc440f51f4f3f
whmyb6 <40862811+whmyb6@users.noreply.github.com>
2019-10-10 22:45:19 +0800

    '更新'
    Signed-off-by: whmyb6 <40862811+whmyb6@users.noreply.github.com>

--- bat/MoveDirectFile.bat
+++ bat/MoveDirectFile.bat
@@ -9,8 +9,8 @@
 @Rem for 循环的工作流程：查找当前文件夹及其子文件夹里面的 txt 文件，找到后把文件路径赋值给变量 %%a，然后执行 do 后面的语句，直到遍历完全部文件。
 @Rem ==========================================================            ====================================================================
 @Rem |                                                        |
-@Rem |          Author: whm           date :2019-10-7         |
-@Rem |                                                        |
+@Rem |          Author: whm           date :2019-10-07        |
+@Rem |                              update :2019-10-10        |
 @Rem ==========================================================
 @Rem
 
@@ -23,6 +23,7 @@ echo curr_path = %curr_path%
 echo curr_disk = %curr_disk%
 
 set work_path=D:\test\
+@Rem set work_path=J:\OK-2017\OK\US\
 
 @Rem 保存当前目录以供 POPD 命令使用，然后改到指定的path 目录
 pushd %work_path%
@@ -48,30 +49,84 @@ goto :end
 @Rem 函数：处理当前目录下的每一个文件，移动目录的子目录下的文件，到当前目录下---
 :listfile
  set curr_work_path=%1%
+ set curr_do_path=%curr_work_path:~1,-1%
+
+ @Rem set curr_do_path=%curr_work_path:^(=%
+ @Rem set curr_do_path=%curr_work_path:^)=%
+ call :strLen curr_do_path,strlen
+
+ @Rem set curr_do_path=%curr_do_path:\=%
+ echo %curr_do_path%
+
+ echo curr_do_path length=%strlen%
+
+ setlocal enabledelayedexpansion
  echo %curr_work_path%
  for /R %curr_work_path% %%s in (*) do (
 	@Rem echo %%s
-	set YYYYmmdd=%date:~0,4%%date:~5,2%%date:~8,2%
-	set hhmiss=%time:~0,2%%time:~3,2%%time:~6,2%
 	@Rem echo %YYYYmmdd%
 	@Rem echo %hhmiss%
 
 	@Rem 取出文件的硬盘+path 信息
 
-	echo filepath=%%~dps  curr_work_path = %curr_work_path:~0,-1%\"
+	@Rem ::echo filepath=%%~dps  curr_work_path=%curr_work_path:~0,-1%\"
+
+	set mfilepath=%%~dps
+	@Rem set mfilepath=!mfilepath:^(=!
+	@Rem set mfilepath=!mfilepath:^)=!
+
+	@Rem set filepath=!filepath:\=!
+	echo mfilepath=!mfilepath! f=%%~dps curr_do_path=%curr_do_path%
+
+	@Rem call echo ffff=%%mfilepath:%curr_do_path%=%%
+    @Rem call set addname=%%mfilepath:%curr_do_path%=%%
+	@Rem set addname=!addname:~1!
+	@Rem set addname=!addname:\=_!
+	call set addname=%%mfilepath:~%strlen%%%
+	set addname=!addname:~1!
+	set addname=!addname:\=_!
+	echo addname=!addname!
+
+
+	@Rem ::set filen=!filepath:~%strlen%!
+	@Rem ::echo !filen!
 
 	@Rem 判断是否为相同目录，文件所在目录与主目录不一致，则进行移动文件的操作。
-	:: %curr_work_path:~0,-1%\"  （说明：删除curr_work_path的最后一个字符,并增加 \ 和 ” 两个字符）
-	if not "%%~dps" == %curr_work_path:~0,-1%\" (
-		if exist %curr_work_path%\%%~ns%%~xs (
-			echo ^^!^^!发现存在同名文件,修改目标文件名称:%%s
-			echo 修改文件名为:%%~ns%YYYYmmdd%_%hhmiss%%%~xs
-			move "%%s" %curr_work_path%\%%~ns%YYYYmmdd%_%hhmiss%%%~xs ) else (
-			echo move file:%%s
-			move "%%s" %curr_work_path% )
+	@Rem :: %curr_work_path:~0,-1%\"  （说明：删除curr_work_path的最后一个字符,并增加 \ 和 ” 两个字符）
+	if not "%%~dps"==%curr_work_path:~0,-1%\" (
+
+		@Rem 关闭变量延迟 ,防止 sfile 无法处理感叹号！！
+		setlocal disabledelayedexpansion
+		set sfile="%%s"
+		setlocal enabledelayedexpansion
+
+		set YYYYmmdd=%date:~0,4%%date:~5,2%%date:~8,2%
+		set hhmiss=%time:~0,2%%time:~3,2%%time:~6,2%
+
+		if exist %curr_work_path%\!addname!%%~ns%%~xs (
+			echo ^^!^^!发现存在同名文件,修改目标文件名称:!sfile!
+			echo 修改文件名为:!addname!%%~ns!YYYYmmdd!_!hhmiss!%%~xs
+			move !sfile! %curr_work_path%\"!addname!%%~ns!YYYYmmdd!_!hhmiss!%%~xs"
+			) else (
+			echo source file=!sfile!
+			set dfile=%curr_work_path%\"!addname!%%~ns%%~xs"
+			echo move file to:!dfile!
+			@Rem move "%%s" %curr_work_path%\"!addname!%%~ns%%~xs"
+			move !sfile! !dfile!
+
+			)
     )
  )
+ endlocal
+ goto :end
+
+ @Rem :: 计算字符串长度 =====
+ @Rem :: call :strLen（函数名） str(字符串) strlen(返回值）
+ :strLen
+ setlocal enabledelayedexpansion
+ :strLen_Loop
+	if not "!%1:~%len%!"=="" set /A len+=1  & goto :strLen_Loop
+ (endlocal & set %2=%len%)
+
 
 :end
-
-
